8b6853741606e5f05b0e8b7c3e26e40311498376
CodeArmer <geanmotas@gmail.com>
2024-12-22 18:46:13 -0400

    Initialized project, added css, added logical initiation

--- client/app.js
+++ client/app.js
@@ -1,1 +1,36 @@
-console.log("Hello World")
+const $app = document.getElementById('app');
+const $board = document.createElement('main')
+$board.id = 'board'
+
+
+const board = [
+    { group: 1, value: [[1, 2, 3], [4, 5, 6], [7, 8, 9]] },
+    { group: 2, value: [[1, 2, 3], [4, 5, 6], [7, 8, 9]] },
+    { group: 3, value: [[1, 2, 3], [4, 5, 6], [7, 8, 9]] },
+    { group: 4, value: [[1, 2, 3], [4, 5, 6], [7, 8, 9]] },
+    { group: 5, value: [[1, 2, 3], [4, 5, 6], [7, 8, 9]] },
+    { group: 6, value: [[1, 2, 3], [4, 5, 6], [7, 8, 9]] },
+    { group: 7, value: [[1, 2, 3], [4, 5, 6], [7, 8, 9]] },
+    { group: 8, value: [[1, 2, 3], [4, 5, 6], [7, 8, 9]] },
+    { group: 9, value: [[1, 2, 3], [4, 5, 6], [7, 8, 9]] },
+]
+
+board.forEach((group, index) => {
+    const $group = document.createElement('section')
+    $group.className = 'group'
+    group.value.forEach(row => {
+        const $row = document.createElement('div')
+        $row.className = 'row'
+        row.forEach(cell => {
+            const $cell = document.createElement('div')
+            const $span = document.createElement('span')
+            $span.innerText = cell
+            $cell.className = 'cell'
+            $cell.appendChild($span)
+            $group.appendChild($cell)
+        })
+    })
+    $board.appendChild($group)
+})
+
+$app.appendChild($board)
---
+++ client/const.js
@@ -0,0 +1,9 @@
+dificulties = {
+    easy: 1,
+    medium: 2,
+    hard: 3,
+    expert: 4,
+    master: 5,
+    legend: 6,
+    devil: 7,
+}
